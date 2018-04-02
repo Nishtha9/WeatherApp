a6a3750621c85284caa8b56589dec4df99181b64
Nishtha9 <Nish0349@gmail.com>
2018-04-02 21:39:48 +0530

    Second

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const request=require('request');
+const yargs=require('yargs');
+const geocode=require('./Geocode/Geocode.js');
 
-request({
-  url:'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA86NJhCdG0jWkJ5opDi-PcHSsdXM-thCE&address=12/34%20malviya%20Nagar%20Jaipur',
-  json:true
-},(error,response,body)=>{
-console.log(`Address: ${body.results[0].formatted_address}`);
-console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-console.log(`Longnitude: ${body.results[0].geometry.location.lng}`);
+const argv=yargs
+.options({
+    a:{
+      damnd:true,
+      alias:'address',
+      description:'Address to fetch weather for'
+    }
+})
+.help()
+.alias('help','h')
+.argv;
+
+geocode.geocodeAddress(argv.address, (errorMsg,results)=>{
+  if (errorMsg){
+    console.log(errorMsg);
+  }
+  else {
+    console.log(JSON.stringify(results,undefined,2));
+  }
 });
