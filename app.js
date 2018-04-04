aeb48b97d288a11fc86c5075d79c1e6bd5d17cfe
Nishtha9 <Nish0349@gmail.com>
2018-04-04 10:34:46 +0530

    Final commit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const request=require('request');
 const yargs=require('yargs');
 const geocode=require('./Geocode/Geocode.js');
+const weather=require('./weather/weather.js');
 
 const argv=yargs
 .options({
     a:{
-      damnd:true,
+      demand:true,
       alias:'address',
       description:'Address to fetch weather for'
     }
@@ -19,6 +20,15 @@ geocode.geocodeAddress(argv.address, (errorMsg,results)=>{
     console.log(errorMsg);
   }
   else {
-    console.log(JSON.stringify(results,undefined,2));
+  console.log(results.Address);
+  weather.getWeather(results.Latitude,results.Longnitude, (errorMsg, weatherResults)=>{
+    if (errorMsg){
+      console.log(errorMsg);
+    }
+    else {
+      console.log(`Its currently ${weatherResults.temp}. It feels like ${weatherResults.appTemp}`);
+    }
+  });
+
   }
 });
